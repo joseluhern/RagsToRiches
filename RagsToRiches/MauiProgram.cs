972560989d8880a8676165d4f37165ba474e566c
Sam Perry <sperry@student.neumont.edu>
2026-08-13 17:03:33 -0600

    Added borders and fixed placement of job and gamble labels

--- RagsToRiches/MauiProgram.cs
+++ RagsToRiches/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace RagsToRiches
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("GrandCasino-Demo.otf", "GrandCasino");
                 });
 
 #if DEBUG
