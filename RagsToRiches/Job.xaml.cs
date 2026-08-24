905358d5742a601f72ed5da69f5248a4c54bae5d
Sam Perry <sperry@student.neumont.edu>
2026-08-24 16:45:53 -0600

    Finished the card randomizer

--- RagsToRiches/Job.xaml.cs
+++ RagsToRiches/Job.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui.Controls;
+using Windows.Media.Audio;
+
 namespace RagsToRiches;
 
 public partial class Job : ContentPage
@@ -7,6 +10,13 @@ public partial class Job : ContentPage
 		InitializeComponent();
 	}
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        CardShuffle();
+    }
+
     private void OnCardClick(object sender, TappedEventArgs e)
     {
 
@@ -14,8 +24,33 @@ public partial class Job : ContentPage
 
 	private void CardShuffle()
 	{
-		Random rand = new Random();
-		List<int> cardValues = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
-		List<int> randCardValues = cardValues.OrderBy(_ => rand.Next()).ToList();
+        int cardListIndex = 0;
+        List<List<int>> gridLocations = new List<List<int>>
+        {
+            new List<int> {0,0},
+            new List<int> {0,1},
+            new List<int> {0,2},
+            new List<int> {0,3},
+            new List<int> {1,0},
+            new List<int> {1,1},
+            new List<int> {1,2},
+            new List<int> {1,3},
+            new List<int> {2,0},
+            new List<int> {2,2}
+        };
+        List<List<int>> randGridLocations = gridLocations.OrderBy(_ => Random.Shared.Next()).ToList();
+        foreach (var child in cardsGrid.Children)
+        {
+            if (child is Image image)
+            {
+                Grid.SetRow(image, randGridLocations[cardListIndex][0]);
+                Grid.SetColumn(image, randGridLocations[cardListIndex][1]);
+                if (randGridLocations[cardListIndex][0] == 2)
+                {
+                    Grid.SetColumnSpan(image, 2);
+                }
+                cardListIndex++;
+            }
+        }
     }
 }
