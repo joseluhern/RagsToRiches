2844cc33b00bc5a73cf12683a2939e34f2fbd201
Sam Perry <sperry@student.neumont.edu>
2026-08-27 16:58:36 -0600

    Finished up card flipping and matching logic

--- RagsToRiches/Job.xaml.cs
+++ RagsToRiches/Job.xaml.cs
@@ -7,7 +7,8 @@ namespace RagsToRiches;
 public partial class Job : ContentPage
 {
     private List<List<int>> cardCoords = new List<List<int>> { };
-	public Job()
+    private bool isLoading = false;
+    public Job()
 	{
 		InitializeComponent();
 	}
@@ -51,21 +52,48 @@ public partial class Job : ContentPage
         }
     }
 
-    private ImageSource GetCardSource(int row, int col)
+    private Image GetImageElement(int row, int col)
     {
-        var imageSource = cardsGrid.Children
+        var image = cardsGrid.Children
+            .OfType<Image>()
             .FirstOrDefault(child => Grid.GetRow((BindableObject)child) == row &&
             Grid.GetColumn((BindableObject)child) == col);
-        if(imageSource is Image image)
+        if(image is Image img)
         {
-            return image.Source;
+            return img;
         }
         return null!;
     }
 
-    private void ImageButton_Clicked(object sender, EventArgs e)
+    private ImageButton GetImageButtonElement(int row, int col)
+    {
+        var imageButton = cardsGrid.Children
+            .OfType<ImageButton>()
+            .FirstOrDefault(child => Grid.GetRow((BindableObject)child) == row &&
+            Grid.GetColumn((BindableObject)child) == col);
+        if (imageButton is ImageButton imgBtn)
+        {
+            return imgBtn;
+        }
+        return null!;
+    }
+
+    private BoxView GetBoxViewElement(int row, int col)
+    {
+        var boxView = cardsGrid.Children
+            .FirstOrDefault(child => Grid.GetRow((BindableObject)child) == row &&
+            Grid.GetColumn((BindableObject)child) == col);
+        if (boxView is BoxView box)
+        {
+            return box;
+        }
+        return null!;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
     {
         var currentCard = sender as ImageButton;
+        if (isLoading) return;
 
         if(currentCard != null)
         {
@@ -76,14 +104,38 @@ public partial class Job : ContentPage
 
                 if (cardCoords.Count == 2)
                 {
-                    if (GetCardSource(cardCoords[0][0], cardCoords[0][1]) == GetCardSource(cardCoords[1][0], cardCoords[1][1]))
+                    isLoading = true;
+                    int imgRow1 = cardCoords[0][0];
+                    int imgCol1 = cardCoords[0][1];
+                    int imgRow2 = cardCoords[1][0];
+                    int imgCol2 = cardCoords[1][1];
+                    await Task.Delay(600);
+                    if (ImageEqualityCheck(GetImageElement(imgRow1, imgCol1), GetImageElement(imgRow2, imgCol2)))
                     {
-                        // Cards match
+                        GetBoxViewElement(imgRow1, imgCol1).IsVisible = false;
+                        GetBoxViewElement(imgRow2, imgCol2).IsVisible = false;
+                        GetImageElement(imgRow1, imgCol1).IsVisible = false;
+                        GetImageElement(imgRow2, imgCol2).IsVisible = false;
                     }
-                    // Make a way for all cards to flip face down again
+                    else
+                    {
+                        GetImageButtonElement(imgRow1, imgCol1).IsVisible = true;
+                        GetImageButtonElement(imgRow2, imgCol2).IsVisible = true;
+                    }
                     cardCoords.Clear();
+                    isLoading = false;
                 }
             }
         }
+    }
+
+    private bool ImageEqualityCheck(Image image1, Image image2)
+    {
+        string GetPath(ImageSource source) => source switch
+        {
+            FileImageSource imageSource => imageSource.File,
+            _ => string.Empty
+        };
+        return GetPath(image1.Source) == GetPath(image2.Source);
     }
 }
